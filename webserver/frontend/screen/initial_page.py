d3fac82437a1fc44d5ac4ae8c212480fcd802d7c
hobbang2 <cattie@pukyong.ac.kr>
2023-01-04 23:07:18 +0000

    refactor: #1 Integrate show function about login, signup, infra

--- webserver/frontend/screen/initial_page.py
+++ webserver/frontend/screen/initial_page.py
@@ -12,58 +12,6 @@ from streamlit_folium import st_folium
 #      = 2 => 둘러보기(인프라 선택)
 #      = 3 => 지도
 
-def show_infra(session:dict):
-
-    st.title('원하는 인프라를 선택하세요 ')
-
-    col1, col2, col3, col4, col5 = st.columns(5)
-
-    with col1:
-        #Infra("편의점", 1, "con_store")
-        st.header("편의점")
-        st.image("./image/편의점.jpeg")
-        if st.checkbox("편의점" , key = 1):
-            con_store = 1
-        else:
-            con_store = 0
-
-    with col2:
-        #Infra("약국", 2, "phar")
-        st.header("약국")
-        st.image("./image/약국.jpeg")
-        if st.checkbox("약국" , key = 2):
-            phar = 1
-        else:
-            phar = 0
-    with col3:
-        #Infra("커피숍", 3, "cafe")
-        st.header("커피숍")
-        st.image("./image/커피숍.jpeg")
-        if st.checkbox("커피숍" , key = 3):
-            cafe = 1
-        else:
-            cafe = 0
-    with col4:
-        #Infra("병원", 4, "hospital")
-        st.header("병원")
-        st.image("./image/병원.jpeg")
-        if st.checkbox("병원" , key = 4):
-            hospital = 1
-        else:
-            hospital = 0
-    with col5:
-    #     Infra("공원", 5)
-        st.header("공원")
-        st.image("./image/공원.jpeg")
-        if st.checkbox("공원" , key = 5):
-            park = 1
-        else:
-            park = 0
-
-    if st.button('제출하기'):
-        session['page_counter'] = 3
-        st.experimental_rerun()
-
 
 def show_login(session:dict):
     # print(os.getcwd())
@@ -84,12 +32,15 @@ def show_login(session:dict):
 
     if True == authentication_status:
         session['page_counter'] = 3
+        st.experimental_rerun()
+
         # return 3 # 지도 화면으로 전환
 
     elif False == authentication_status :
         st.error('아이디 혹은 비밀번호가 틀렸습니다.')
 
     col1, col2 = st.columns(2)
+    print(session['page_counter'])
 
     with col1:
         if st.button("회원가입"):
@@ -160,3 +111,56 @@ def show_signup(session:dict):
                 session['page_counter'] = 2
                 st.experimental_rerun()
 
+
+
+def show_infra(session:dict):
+
+    st.title('원하는 인프라를 선택하세요 ')
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        #Infra("편의점", 1, "con_store")
+        st.header("편의점")
+        st.image("./image/편의점.jpeg")
+        if st.checkbox("편의점" , key = 1):
+            con_store = 1
+        else:
+            con_store = 0
+
+    with col2:
+        #Infra("약국", 2, "phar")
+        st.header("약국")
+        st.image("./image/약국.jpeg")
+        if st.checkbox("약국" , key = 2):
+            phar = 1
+        else:
+            phar = 0
+    with col3:
+        #Infra("커피숍", 3, "cafe")
+        st.header("커피숍")
+        st.image("./image/커피숍.jpeg")
+        if st.checkbox("커피숍" , key = 3):
+            cafe = 1
+        else:
+            cafe = 0
+    with col4:
+        #Infra("병원", 4, "hospital")
+        st.header("병원")
+        st.image("./image/병원.jpeg")
+        if st.checkbox("병원" , key = 4):
+            hospital = 1
+        else:
+            hospital = 0
+    with col5:
+    #     Infra("공원", 5)
+        st.header("공원")
+        st.image("./image/공원.jpeg")
+        if st.checkbox("공원" , key = 5):
+            park = 1
+        else:
+            park = 0
+
+    if st.button('제출하기'):
+        session['page_counter'] = 3
+        st.experimental_rerun()
