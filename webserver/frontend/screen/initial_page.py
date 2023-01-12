e069638dfb77df0d0034f505bb6a45ed5a4e0d37
gitwoo <jhu8802@naver.com>
2023-01-12 23:39:00 +0900

    feat : Infra select more than three when submit

--- webserver/frontend/screen/initial_page.py
+++ webserver/frontend/screen/initial_page.py
@@ -130,24 +130,23 @@ def show_signup(session:dict):
 
 
 
+
+
 def show_infra(session:dict):
 
     locat = ['강남구','강동구','강서구','강북구','관악구','광진구','구로구','금천구','노원구',
     '동대문구','도봉구','동작구','마포구','서대문구','성동구','성북구','서초구','송파구',
     '영등포구','용산구','양천구','은평구','종로구','중구','중랑구']
-    st.subheader('희망 거주 지역을 선택하세요 ')
-    locate = st.selectbox('',locat)
-    st.subheader('원하는 인프라를 선택하세요 (3개 이상) ')
+    st.title('희망 거주 지역을 선택하세요 ')
+    locate = st.selectbox(' ',locat)
 
-    st.title('원하는 인프라를 선택하세요 ')
+    st.title('원하는 인프라를 선택하세요 (3개 이상) ')
 
     col1, col2, col3, col4 = st.columns(4)
     col5, col6, col7, col8 = st.columns(4)
     check_cnt = 0
 
-
     with col1:
-        #Infra("편의점", 1, "con_store")
         st.header("편의점")
         st.image("./image/편의점.png")
         if st.checkbox("편의점" , key = 1):
@@ -155,10 +154,8 @@ def show_infra(session:dict):
             check_cnt += 1
         else:
             con_store = 0
-            check_cnt -= 1
-
+            
     with col2:
-        #Infra("약국", 2, "phar")
         st.header("약국")
         st.image("./image/약국.png")
         if st.checkbox("약국" , key = 2):
@@ -166,9 +163,8 @@ def show_infra(session:dict):
             check_cnt += 1
         else:
             phar = 0
-            check_cnt -= 1
+
     with col3:
-        #Infra("커피숍", 3, "cafe")
         st.header("카페")
         st.image("./image/카페.png")
         if st.checkbox("카페" , key = 3):
@@ -176,50 +172,69 @@ def show_infra(session:dict):
             check_cnt += 1
         else:
             cafe = 0
-            check_cnt -= 1
+
     with col4:
-        #Infra("병원", 4, "hospital")
         st.header("병원")
         st.image("./image/병원.png")
         if st.checkbox("병원" , key = 4):
             hospital = 1
+            check_cnt += 1
         else:
             hospital = 0
+
     with col5:
-    #     Infra("공원", 5)
         st.header("공원")
         st.image("./image/공원.png")
         if st.checkbox("공원" , key = 5):
             park = 1
+            check_cnt += 1
         else:
             park = 0
+
     with col6:
-    #     Infra("공원", 5)
         st.header("대형마트")
         st.image("./image/대형마트.png")
         if st.checkbox("대형마트" , key = 6):
-            park = 1
+            mart = 1
+            check_cnt += 1
         else:
-            park = 0
+            mart = 0
+
     with col7:
-    #     Infra("공원", 5)
         st.header("영화관")
         st.image("./image/영화관.png")
         if st.checkbox("영화관" , key = 7):
-            park = 1
+            theater = 1
+            check_cnt += 1
         else:
-            park = 0
+            theater = 0
+
     with col8:
-    #     Infra("공원", 5)
         st.header("지하철")
         st.image("./image/지하철.png")
         if st.checkbox("지하철" , key = 8):
-            park = 1
+            subway = 1
+            check_cnt += 1
         else:
-            park = 0
-    if st.button('제출하기'):
-        session['page_counter'] = 3
+            subway = 0
 
+    def disable():  
+        session["disabled"] = True
+
+
+    if check_cnt >= 3:
+        if st.button('제출하기'):
+            session['page_counter'] = 3
+            print(check_cnt)
+            st.experimental_rerun()
+    else:
+        st.button('제출하기', disabled = True)
+        print(check_cnt)
+
+
+
+
+        
         # url = 'http://27.96.130.120:30002/infra'
         # infra = {
         #     "user_id" : 6,
@@ -228,4 +243,4 @@ def show_infra(session:dict):
         # x = requests.post(url,data=json.dumps(infra))
         # print(x)
 
-        st.experimental_rerun()
+        
