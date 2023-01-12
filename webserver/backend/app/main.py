60cc10380d1131d8f11c8664668b525b2a1c17b8
hyein99 <hyeinnn999@gmail.com>
2023-01-12 12:26:30 +0000

    feat : Modify API structure Change 'routes' to 'routers' and make router to api function

--- webserver/backend/app/main.py
+++ webserver/backend/app/main.py
@@ -3,7 +3,7 @@
 import os
 from typing import Optional
 from fastapi import FastAPI
-from routes.test import router as test_router
+from routers.test import router as test_router
 
 app = FastAPI() # FastAPI 모듈
 app.include_router(test_router) # 다른 route파일들을 불러와 포함시킴
