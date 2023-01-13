8c84b82eedb841e7e6a08b4e40106c08486f1864
hyein99 <hyeinnn999@gmail.com>
2023-01-13 08:46:59 +0000

    fix : #6 Fix bug which make hobbang_model import error Change model names and add models

--- webserver/backend/app/crud/hobbang_crud_test.py
+++ webserver/backend/app/crud/hobbang_crud_test.py
@@ -1,7 +1,7 @@
 # crud/hobbang_crud_test.py
 
 from sqlalchemy.orm import Session
-from db.models.hobbang_model import HobbangTest
+from db.models.hobbang_model import HouseInfo, UsersInfo, UsersInfra, UserZzim, Infra, InfraInfo, Grid, GridScore, LogClick
 
 def get_infra(db: Session):
-    return db.query(HobbangTest).all()
+    return db.query(Infra).all()
