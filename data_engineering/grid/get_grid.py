dc44e3f186aab79868ddde11074febccf3399395
hyunho <hyunho656@gmail.com>
2023-01-19 01:13:05 +0000

    feat : #8 Add closest infra_id, infra_lat, infra_lng in getting grid

--- data_engineering/grid/get_grid.py
+++ data_engineering/grid/get_grid.py
@@ -20,7 +20,7 @@ def make_grid(map_min_lat: float, map_max_lat: float, map_min_long: float, map_m
     x_itv = (map_max_long - map_min_long)/w_box
     y_itv = (map_max_lat - map_min_lat)/h_box
     
-    grid_df = pd.DataFrame({'id':[],'main_long':[],'main_lat':[],'min_long':[],'max_long':[],'min_lat':[],'max_lat':[]})
+    grid_df = pd.DataFrame({'id':[],'main_lat':[],'main_long':[],'min_lat':[],'max_lat':[],'min_long':[],'max_long':[]})
     start = time.time()
     for i_y,k_y in enumerate(tqdm(np.arange(map_min_lat, map_max_lat,y_itv))):
         for i_x,k_x in enumerate(np.arange(map_min_long, map_max_long,x_itv)):
@@ -39,20 +39,40 @@ def make_infra_columns(grid_df: pd.DataFrame, infra_df: pd.DataFrame, infra_name
     
     print(f"make_{infra_name}_columns start!")  
     
+    now = time.localtime()
+    now_date = time.strftime('%Y%m%d', now)
+    now_hour = time.strftime('%X', now)
+    save_time = now_date + '_' + now_hour.replace(':', '')
+
     start = time.time()
     for index_g, row_g in tqdm(grid_df.iterrows(), total=grid_df.shape[0]):
         min_dist = 1000000
         cnt = 0
+        
+        infra_long = -1
+        infra_lat = -1
+        infra_id = '-1'
     
+        if index_g%100000 == 0:
+            grid_df.to_csv('grid_' + save_time + '_' + str(index_g) + '_with'+infra_name+'.csv', index=False)
+
         filterd_infra_df = infra_df[(row_g['main_lat']-radius*1.1*lat_km<infra_df['위도']) & (infra_df['위도']<row_g['main_lat']+radius*1.1*lat_km) & (row_g['main_long']-radius*1.1*long_km<infra_df['경도']) & (infra_df['경도']<row_g['main_long']+radius*1.1*long_km)]
         for index_t, row_t in filterd_infra_df.iterrows():
             tmp = haversine((row_g['main_lat'],row_g['main_long']),(row_t['위도'],row_t['경도']))
             if tmp < radius: 
                 cnt+=1
                 min_dist = min(min_dist, tmp)
+                infra_long = row_t['경도']
+                infra_lat = row_t['위도']
+                infra_id = row_t['infra_id']
+        grid_df.loc[index_g,infra_name + '_id']=infra_id
+        grid_df.loc[index_g,infra_name + '_lat']=infra_lat
+        grid_df.loc[index_g,infra_name + '_long']=infra_long
         grid_df.loc[index_g,infra_name + '_dist']=min_dist
         grid_df.loc[index_g,infra_name + '_cnt']=cnt
         
+    # grid[infra_name + '_cnt'] = grid[infra_name + '_cnt'].apply(lambda x: str(x))
+    
     end = time.time()
     grid_df.loc[grid_df[infra_name + '_dist']==1000000,infra_name + '_dist']=-1
     print(f"make_{infra_name}_columns: {end - start:.5f} sec")
@@ -81,13 +101,21 @@ def main(args):
     print("main start!")
     start = time.time()
     ## Data load
-    park = pd.read_csv(args.DATA_PATH + 'park_data_v1.1.csv')
-    mart = pd.read_csv(args.DATA_PATH + 'mart_data.csv')
-    cs = pd.read_csv(args.DATA_PATH + 'cs_data.csv')
-    cafe = pd.read_csv(args.DATA_PATH + 'cafe_data_v1.1.csv')
-    pharmacy = pd.read_csv(args.DATA_PATH + 'pharmacy_data_v2.0.csv')
-    subway = pd.read_csv(args.DATA_PATH + 'subway_data_v1.2.csv')
-    theater = pd.read_csv(args.DATA_PATH + 'theater_data_v1.2.csv')
+    park = pd.read_csv(args.DATA_PATH + 'park_data_v3.0.csv')
+    mart = pd.read_csv(args.DATA_PATH + 'mart_data_v3.0.csv')
+    cs = pd.read_csv(args.DATA_PATH + 'cs_data_v3.0.csv')
+    cafe = pd.read_csv(args.DATA_PATH + 'cafe_data_v3.0.csv')
+    pharmacy = pd.read_csv(args.DATA_PATH + 'pharmacy_data_v3.0.csv')
+    subway = pd.read_csv(args.DATA_PATH + 'subway_data_v3.0.csv')
+    theater = pd.read_csv(args.DATA_PATH + 'theater_data_v3.0.csv')
+    
+    park['infra_id'] = park['infra_id'].apply(lambda x: str(x).zfill(8))
+    mart['infra_id'] = mart['infra_id'].apply(lambda x: str(x).zfill(8))
+    cs['infra_id'] = cs['infra_id'].apply(lambda x: str(x).zfill(8))
+    cafe['infra_id'] = cafe['infra_id'].apply(lambda x: str(x).zfill(8))
+    pharmacy['infra_id'] = pharmacy['infra_id'].apply(lambda x: str(x).zfill(8))
+    subway['infra_id'] = subway['infra_id'].apply(lambda x: str(x).zfill(8))
+    theater['infra_id'] = theater['infra_id'].apply(lambda x: str(x).zfill(8))
     
     grid_df = make_grid(args.COORD[0], args.COORD[1], args.COORD[2], args.COORD[3])
 
@@ -97,28 +125,36 @@ def main(args):
     save_time = now_date + '_' + now_hour.replace(':', '')
     grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withoutInfra.csv', index=False)
     
-    grid_df = make_infra_columns(grid_df,theater,'theater',2)
-    grid_df = make_score_columns(grid_df,'theater',2)
+    # grid_df = pd.read_csv('./output/grid_20230111_200538_withoutInfra.csv')
 
-    # grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withTheater.csv', index=False)    
+    grid_df = make_infra_columns(grid_df,subway,'subway',0.3)
+    grid_df = make_score_columns(grid_df,'subway',0.3)
+    grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withSubway.csv', index=False) 
 
-    grid_df = make_infra_columns(grid_df,pharmacy,'phar',0.3)
-    grid_df = make_score_columns(grid_df,'phar',0.3)
-
-    # grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withPhar.csv', index=False) 
-
-    grid_df = make_infra_columns(grid_df,subway,'subway',0.5)
-    grid_df = make_score_columns(grid_df,'subway',0.5)
-
-    # grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withSubway.csv', index=False) 
+    grid_df = make_infra_columns(grid_df,cs,'cs',0.1)
+    grid_df = make_score_columns(grid_df,'cs',0.1)
+    grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withCs.csv', index=False)
+    
+    grid_df = make_infra_columns(grid_df,mart,'mart',0.5)
+    grid_df = make_score_columns(grid_df,'mart',0.5)
+    grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withMart.csv', index=False)
+    
+    grid_df = make_infra_columns(grid_df,park,'park',0.3)
+    grid_df = make_score_columns(grid_df,'park',0.3)
+    grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withPark.csv', index=False) 
 
     grid_df = make_infra_columns(grid_df,cafe,'cafe',0.3)
     grid_df = make_score_columns(grid_df,'cafe',0.3)
+    grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withCafe.csv', index=False) 
 
-    # grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withCafe.csv', index=False) 
+    grid_df = make_infra_columns(grid_df,pharmacy,'phar',0.3)
+    grid_df = make_score_columns(grid_df,'phar',0.3)
+    grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withPhar.csv', index=False) 
+    
+    grid_df = make_infra_columns(grid_df,theater,'theater',1)
+    grid_df = make_score_columns(grid_df,'theater',1)
+    grid_df.to_csv(args.OUTPUT_PATH + 'grid_' + save_time + '_withTheater.csv', index=False)    
 
-    grid_df = make_infra_columns(grid_df,cs,'cs',0.3)
-    grid_df = make_score_columns(grid_df,'cs',0.3)
     
     now = time.localtime()
     now_date = time.strftime('%Y%m%d', now)
