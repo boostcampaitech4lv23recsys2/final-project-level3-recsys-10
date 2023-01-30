29caeeb6783081a631c333ba1c99d948bf88b02e
kuuneeee <rjsdud0586@gmail.com>
2023-01-30 17:29:34 +0000

    fix: Fix error by wrong notation

--- modeling/RecBole/recbole/utils/utils.py
+++ modeling/RecBole/recbole/utils/utils.py
@@ -64,7 +64,7 @@ def get_model(model_name):
     """
     model_submodule = [
         "general_recommender",
-        # "context_aware_recommender",
+        "context_aware_recommender",
         # "sequential_recommender",
         # "knowledge_aware_recommender",
         # "exlib_recommender",
