5b43071056b46b430188512fae42e7c04dbfd25e
hobbang2 <cattie@pukyong.ac.kr>
2023-02-04 23:24:08 +0000

    feat: Add marker reducer to app state

--- frontend/src/store/AppState.ts
+++ frontend/src/store/AppState.ts
@@ -1,7 +1,9 @@
 import * as User from "./user";
 import * as House from "./house";
+import * as Marker from "./marker";
 
 export type AppState = {
   user: User.State;
   house: House.State;
+  marker: Marker.State;
 };
