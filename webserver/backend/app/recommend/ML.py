531a35b0455bd100da137ce001ffac4c8766dba0
kuuneeee <rjsdud0586@gmail.com>
2023-02-06 09:52:36 +0000

    feat: Delete score_4 infra and Fix notation in inference_ml code

--- webserver/backend/app/recommend/ML.py
+++ webserver/backend/app/recommend/ML.py
@@ -29,7 +29,6 @@ def train_ml_gu(user_id, user_gu, all_user, all_house, zzim_list, click, db: Ses
                     , CASE WHEN (SELECT COUNT(*) FROM USERS_INFRA WHERE user_id={user_id} AND infra_type = '01' AND infra_yn = 'Y') > 0 THEN G1.infra_dist_score ELSE 0 END as score_1
                     , CASE WHEN (SELECT COUNT(*) FROM USERS_INFRA WHERE user_id={user_id} AND infra_type = '02' AND infra_yn = 'Y') > 0 THEN G2.infra_dist_score ELSE 0 END as score_2
                     , CASE WHEN (SELECT COUNT(*) FROM USERS_INFRA WHERE user_id={user_id} AND infra_type = '03' AND infra_yn = 'Y') > 0 THEN G3.infra_dist_score ELSE 0 END as score_3
-                    , CASE WHEN (SELECT COUNT(*) FROM USERS_INFRA WHERE user_id={user_id} AND infra_type = '04' AND infra_yn = 'Y') > 0 THEN G4.infra_dist_score ELSE 0 END as score_4
                     , CASE WHEN (SELECT COUNT(*) FROM USERS_INFRA WHERE user_id={user_id} AND infra_type = '05' AND infra_yn = 'Y') > 0 THEN G5.infra_dist_score ELSE 0 END as score_5
                     , CASE WHEN (SELECT COUNT(*) FROM USERS_INFRA WHERE user_id={user_id} AND infra_type = '06' AND infra_yn = 'Y') > 0 THEN G6.infra_dist_score ELSE 0 END as score_6
                     , CASE WHEN (SELECT COUNT(*) FROM USERS_INFRA WHERE user_id={user_id} AND infra_type = '07' AND infra_yn = 'Y') > 0 THEN G7.infra_dist_score ELSE 0 END as score_7
@@ -44,9 +43,6 @@ def train_ml_gu(user_id, user_gu, all_user, all_house, zzim_list, click, db: Ses
             LEFT JOIN GRID_SCORE G3
                     ON (H.grid_id = G3.grid_id
                         AND G3.infra_type = '03')
-            LEFT JOIN GRID_SCORE G4
-                    ON (H.grid_id = G4.grid_id
-                        AND G4.infra_type = '04')
             LEFT JOIN GRID_SCORE G5
                     ON (H.grid_id = G5.grid_id
                         AND G5.infra_type = '05')
@@ -70,12 +66,12 @@ def train_ml_gu(user_id, user_gu, all_user, all_house, zzim_list, click, db: Ses
                             , columns=['house_id', 'grid_id', 'sales_type', 'service_type'
                                         , 'house_area', 'price_deposit', 'price_monthly_rent', 'manage_cost'
                                         , 'address', 'local2'
-                                        , 'score_1', 'score_2', 'score_3', 'score_4', 'score_5', 'score_6', 'score_7', 'score_8'])
+                                        , 'score_1', 'score_2', 'score_3', 'score_5', 'score_6', 'score_7', 'score_8'])
    
     h_info.columns = ['house_id:token', 'grid_id:token', 'sales_type:token', 'service_type:token'
                     , 'house_area:float', 'price_deposit:float', 'price_monthly_rent:float', 'manage_cost:float'
                     , 'address:token', 'local2:token'
-                    , 'score_1:float', 'score_2:float', 'score_3:float', 'score_4:float', 'score_5:float', 'score_6:float', 'score_7:float', 'score_8:float']
+                    , 'score_1:float', 'score_2:float', 'score_3:float', 'score_5:float', 'score_6:float', 'score_7:float', 'score_8:float']
     
     # zzim
     zzim_info = pd.DataFrame(zzim_list, columns=['idx','user_id','house_id','register_date','update_date','zzim_yn'])
@@ -237,26 +233,26 @@ def train_ml_latlng(user_id, min_lat, max_lat, min_lng, max_lng, all_user, all_h
 
 
 def inference_ml(zzim_list, click, db: Session):
-    # zzim
-    zzim_info = pd.DataFrame(zzim_list, columns=['idx','user_id','house_id','register_date','update_date','zzim_yn'])
-    zzim_info['interest'] = 0
-    zzim_info.loc[zzim_info[zzim_info['zzim_yn']=='Y'].index, 'interest'] = 5
-    zzim_info.loc[zzim_info[zzim_info['zzim_yn']=='N'].index, 'interest'] = 1
-    zzim_info = zzim_info[['user_id', 'house_id','interest']]
-    zzim_info.columns = ['user_id:token', 'house_id:token', 'interest:float']
+    # # zzim
+    # zzim_info = pd.DataFrame(zzim_list, columns=['idx','user_id','house_id','register_date','update_date','zzim_yn'])
+    # zzim_info['interest'] = 0
+    # zzim_info.loc[zzim_info[zzim_info['zzim_yn']=='Y'].index, 'interest'] = 5
+    # zzim_info.loc[zzim_info[zzim_info['zzim_yn']=='N'].index, 'interest'] = 1
+    # zzim_info = zzim_info[['user_id', 'house_id','interest']]
+    # zzim_info.columns = ['user_id:token', 'house_id:token', 'interest:float']
     
-    # click
-    click_info = pd.DataFrame(click, columns=['idx','ip_address','user_id', 'item_id', 'log_type', 'log_date'])
-    click_info['interest'] = 3
-    click_info = click_info[['user_id', 'item_id', 'interest']]
-    click_info.columns = ['user_id:token', 'house_id:token', 'interest:float']
-    interaction = pd.concat([zzim_info, click_info])
+    # # click
+    # click_info = pd.DataFrame(click, columns=['idx','ip_address','user_id', 'item_id', 'log_type', 'log_date'])
+    # click_info['interest'] = 3
+    # click_info = click_info[['user_id', 'item_id', 'interest']]
+    # click_info.columns = ['user_id:token', 'house_id:token', 'interest:float']
+    # interaction = pd.concat([zzim_info, click_info])
     
-    # save
-    path = '/opt/ml/final-project-level3-recsys-10/modeling/RecBole/dataset/rec/'
-    interaction.to_csv(path + 'rec.inter', index=False, sep='\t')
+    # # save
+    # path = '/opt/ml/final-project-level3-recsys-10/modeling/RecBole/dataset/rec/'
+    # interaction.to_csv(path + 'rec.inter', index=False, sep='\t')
     
-    result = inference()
+    result1, result2, result3 = inference()
 
-    return result
+    return result1, result2, result3
 
